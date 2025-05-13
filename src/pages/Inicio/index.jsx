a973e93e91097debc9579b99a7406b93c8732a00
Pauloglhr <pauloglhr@outlook.com>
2025-05-12 21:01:52 -0300

    Desenvolvido API de consulta externa para realizar requisições dos vídeos

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -1,11 +1,18 @@
+import { useEffect, useState } from "react";
 import { Banner } from "../../components/Banner";
 import Card from "../../components/Card";
 import Titulo from "../../components/Titulo";
-
-import videos from "../../db.json";
 import styles from "./Inicio.module.css";
 
 const Inicio = () => {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetch('https://my-json-server.typicode.com/pauloglhr/cinetag-api/videos')
+    .then(resposta => resposta.json())
+    .then(dados => {
+      setVideos(dados)
+    })
+  },[])
   return (
     <>
       <Banner imagem={"home"} />
--- src/pages/Player/index.jsx
+++ src/pages/Player/index.jsx
@@ -1,16 +1,21 @@
 import { useParams } from "react-router-dom";
 import { Banner } from "../../components/Banner";
 import Titulo from "../../components/Titulo";
-import videos from "../../db.json";
 import styles from "./Player.module.css";
 import NaoEcontrada from "../NaoEcontrada";
+import { useEffect, useState } from "react";
 
 const Player = () => {
+  const [video, setVideo] = useState();
   const parametros = useParams();
-  const video = videos.find((video) => {
-    return video.id === Number(parametros.id);
-  });
-
+  useEffect(() => {
+    fetch(`https://my-json-server.typicode.com/pauloglhr/cinetag-api/videos?${parametros.id}`)
+    .then(res => res.json())
+    .then(dados => {
+      setVideo(...dados)
+    })
+  }, [])
+  
   if(!video){
     return <NaoEcontrada />
   }
